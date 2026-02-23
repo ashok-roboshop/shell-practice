dbdea866776e933d38b78a0dacf46b337960894d
AshokReddy <arigaashokreddy@gmail.com>
2026-02-23 12:20:10 +1100

    shell script

---
+++ 10-install-command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+  echo "ERROR:: please run this script with root access"
+else 
+  echo "you run with root access"
+fi
+
+dnf install mysql -y
