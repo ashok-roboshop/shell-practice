15117e976b2a482bad61ae75eda9ee2f98f104a1
AshokReddy <arigaashokreddy@gmail.com>
2026-02-23 11:25:19 +1100

    shell script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $@"
+echo "Number of variables: $#"
+echo "script name: $0"
+echo "current Directory: $PWD"
+echo " User running thid script: $USER"
+echo "Home directory of user : $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
